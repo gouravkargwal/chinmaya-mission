3bd596ba9557945a68ea51ca0a3659462db2b86d
Gourav Kargwal <gouravkargwalverma@gmail.com>
2022-10-14 22:14:53 +0530

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome!</h1>
+        <p className="red">lad asd asdas as d</p>
+      </div>
+      <h1 className="red">Hello!</h1>
+    </>
+  );
+}
+
+export default App;
